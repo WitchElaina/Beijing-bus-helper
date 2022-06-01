6d41ecd27728e51c3513c7e78746e07e539e45dc
WitchElaina <msz13399173387@gmail.com>
2022-06-01 14:04:51 +0800

    Add txt praser

---
+++ prase.py
@@ -0,0 +1,34 @@
+"""
+prase.py
+用于从st.txt文档中提取站点信息, 并将其转化为python对象
+"""
+
+filename = ''
+
+
+def load(f_name: str):
+    """
+    初始化函数, 设置数据源的路径和文件名
+    :param f_name: 数据源文件名
+    :return: None
+    """
+    global filename
+    filename = f_name
+
+
+def to_dict():
+    """
+    将txt转化为字典并输出, 键为线路名称, 值为线路站点列表
+    :return: 包含所有线路信息的字典
+    """
+    # TODO
+    pass
+
+
+def to_list():
+    """
+    将txt中所有站点存放在列表中, 返回一个包含所有站点且无重复站点的列表
+    :return: 包含所有线路信息的字典
+    """
+    # TODO
+    pass
