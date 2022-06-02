162373b6ac638ffba14e610a53b4cb97d121a0c8
udhuaioknoiug <1398558451@qq.com>
2022-06-02 11:37:19 +0800

    Add codes

--- prase.py
+++ prase.py
@@ -21,8 +21,14 @@ def to_dict():
     将txt转化为字典并输出, 键为线路名称, 值为线路站点列表
     :return: 包含所有线路信息的字典
     """
-    # TODO
-    pass
+    content = {}
+    f1 = open(filename, 'rt', encoding="utf-8")
+    for line in f1:
+        key = line[:line.index('-')]
+        value = line[line.index('[')+2:-3].split("', '")
+        content[key] = value
+    f1.close()
+    return content
 
 
 def to_list():
@@ -30,8 +36,14 @@ def to_list():
     将txt中所有站点存放在列表中, 返回一个包含所有站点且无重复站点的列表
     :return: 包含所有线路信息的字典
     """
-    # TODO
-    pass
+    content = []
+    f1 = open(filename, 'rt', encoding="utf-8")
+    for line in f1:
+        value = line[line.index('[')+2:-3].split("', '")
+        content.extend(value)
+    f1.close()
+    content = list(set(content))
+    return content
 
 
 def to_adj_list():
